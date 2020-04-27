a2654e60e2685a6ea3921d5f8bd4f0103fd5dd56
ryota kise <ryota1839@gmail.com>
2020-04-27 21:31:14 +0900

    ヘッダーのレスポンシブを整えた

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,40 @@
 <template>
   <div>
     <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Trade brand memo</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <template>
-        <div class="my-2">
-          <v-btn to="/memoHome" text x-large>Home</v-btn>
-        </div>
-        <div class="my-2">
-          <v-btn text x-large>Search</v-btn>
-        </div>
-        <div class="my-2">
-          <v-btn to="/memoAdd" text x-large>Add</v-btn>
-        </div>
-        <div class="my-2">
-          <v-btn to="/" text x-large>logout</v-btn>
-        </div>
-      </template>
-      <template>
-        <div class="my-2">
-          <v-btn to="/" text x-large>Login</v-btn>
-        </div>
-        <div class="my-2">
-          <v-btn to="/memoRegister" text x-large>Register</v-btn>
-        </div>
-      </template>
+      <v-container>
+        <v-layout wrap>
+          <v-flex row xs12 sm6 md6>
+            <v-toolbar-title class="mt-5 headline text-uppercase">
+              <span>Trade brand memo</span>
+            </v-toolbar-title>
+          </v-flex>
+          <v-spacer/>
+          <v-flex row xs12 sm6 md6>
+            <template>
+              <div class="my-2">
+                <v-btn to="/memoHome" text x-large>Home</v-btn>
+              </div>
+              <div class="my-2">
+                <v-btn text x-large>Search</v-btn>
+              </div>
+              <div class="my-2">
+                <v-btn to="/memoAdd" text x-large>Add</v-btn>
+              </div>
+              <!-- <div class="my-2">
+                <v-btn to="/" text x-large>logout</v-btn>
+              </div> -->
+            </template>
+            <template>
+              <div class="my-2">
+                <v-btn to="/" text x-large>Login</v-btn>
+              </div>
+              <div class="my-2">
+                <v-btn to="/memoRegister" text x-large>Register</v-btn>
+              </div>
+            </template>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-app-bar>
   </div>
 </template>
