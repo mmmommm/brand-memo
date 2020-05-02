242348f590b56b35c9ac53c75cc964f3945bf807
ryota kise <ryota1839@gmail.com>
2020-05-03 07:00:55 +0900

    一致したドキュメントを取り出せるようにした

--- src/components/memoSearch.vue
+++ src/components/memoSearch.vue
@@ -1,22 +1,22 @@
 <template>
   <v-container>
     <v-layout>
-      <v-text-field v-model.number="hint" placeholder="search brand by code"/>
-      <v-btn @click.prevent="filteredList()">検索</v-btn>
+      <v-text-field v-model.number="search_term" placeholder="search brand by code"/>
+      <v-btn @click="filteredList()">検索</v-btn>
     </v-layout>
     <v-layout wrap>
-        <v-flex xs12 sm6 md4 v-for="(memo, index) in filteredList" :key="index">
-          <v-card width="350" height="200" class="my-5">
-            <v-card-title class="ml-2">{{ memo.code }}</v-card-title>
-            <v-card-title class="ml-2">{{ memo.name }}</v-card-title>
-            <v-card-text>
-              <v-layout>
-                <v-btn outlined class="button ml-2 mt-6" :to="{ name: 'memo-detail', params: { memo: memo.slug }}">viewmemo</v-btn>
-              </v-layout>
-            </v-card-text>
-          </v-card>
-        </v-flex>
-      </v-layout>
+      <v-flex xs12 sm6 md4 v-for="(memo, index) in filteredList" :key="index">
+        <v-card width="350" height="200" class="my-5">
+          <v-card-title class="ml-2">{{ memo.code }}</v-card-title>
+          <v-card-title class="ml-2">{{ memo.name }}</v-card-title>
+          <v-card-text>
+            <v-layout>
+              <v-btn outlined class="button ml-2 mt-6" :to="{ name: 'memo-detail', params: { memo: memo.slug }}">viewmemo</v-btn>
+            </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 <script lang="ts">
@@ -25,11 +25,12 @@ import { firestore } from '@/firebase/fireStore';
 import { MemoItem } from '@/interface/memoItem';
 @Component
 export default class MemoSearch extends Vue {
-hint: number | null = null
+search_term: number | null = null
 code: number | null = 0
 name: string | null = ""
-search = 0
 memos: MemoItem[] = []
+// filteredList: MemoItem[] = []
+// codeList: (number | null)[] = []
   created() {
     // eslint-disable-next-line
     firestore.collection('memos').get().then((querySnapshot: any) => {
@@ -39,13 +40,24 @@ memos: MemoItem[] = []
         array.push(doc.data())
       });
         this.memos = array
+        // arrayにぶちこまれてるmemosからそれぞれのmemoのcodeの値を取りだしてcodeListに入れる
+      // array.forEach((s) => {
+      //   this.codeList.push(s.code)
+      // })
     });
   }
-  filteredList(): MemoItem[] | null {
-    if (!this.hint) return null;
-     return this.memos.filter(memo => {
-      memo.code.includes(this.search)
-     })
+  filteredList() {
+    firestore.collection('memos').where("code", "==", this.search_term).get()
+    .then(() => {
+      console.log("成功です")
+    })
+    .catch(() => {
+      console.error("失敗です")
+      return this.memos
+    })
+    //  return this.memos.filter(t => {
+    //   this.search_term === this.memos. ?
+    //  })
   }
 }
 </script>
