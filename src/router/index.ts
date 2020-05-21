8c216599cf504a2202220e195df81e51c7966079
ryota kise <ryota1839@gmail.com>
2020-05-22 02:56:42 +0900

    ルートメタフィールドを追記

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,24 +8,29 @@ import memoEdit from '@/views/memo/memoEdit.vue';
 import memoDetail from '@/views/memo/memoDetail.vue';
 import PageNotFound from '@/views/PageNotFound.vue';
 import memoSearch from '@/views/memo/memoSearch.vue';
+import store from 'vuex'
 
 Vue.use(VueRouter)
-
 const routes: Array<RouteConfig> = [
+  //共通で表示するもの
+  {
+    path: '/memoHome',
+    component: memoHome,
+  },
+  {
+    path: '/memoSearch',
+    component: memoSearch,
+  },
+  //ログインしていないとガードするもの
   {
     path: '/memoLogin',
     component: memoLogin,
-    // beforeEnter(to, from, next) {
-    //   if (store.getters.idToken) {
-    //     next('/memoHome');
-    //   } else {
-    //     next();
-    //   }
-    // }
+    meta: { requiresAuth: true }
   },
   {
     path: '/memoAdd',
     component: memoAdd,
+    meta: { requiresAuth: true }
     // beforeEnter(to, from, next) {
     //   if(store.getters.idToken) {
     //     next();
@@ -35,19 +40,9 @@ const routes: Array<RouteConfig> = [
     // }
   },
   {
-    path: '/memoHome',
-    component: memoHome,
-    // beforeEnter(to, from, next) {
-    //   if (store.getters.idToken) {
-    //     next();
-    //   } else {
-    //     next('/');
-    //   }
-    // }
-  },
-  {
     path: '/memoRegister',
     component: memoRegister,
+    meta: { requiresAuth: true }
     // beforeEnter(to, from, next) {
     //   if (store.getters.idToken) {
     //     next('/memoHome');
@@ -57,13 +52,10 @@ const routes: Array<RouteConfig> = [
     // }
   },
   {
-    path: '/memoSearch',
-    component: memoSearch,
-  },
-  {
     path: '/:memo',
     name: 'memo-detail',
     component: memoDetail,
+    meta: { requiresAuth: true },
     children: [
       {
         path: '/edit',
@@ -79,7 +71,7 @@ const routes: Array<RouteConfig> = [
     //   }
     // }
   },
-  //上の動的ルーティングに吸われて機能しない
+  //上の動的ルーティングに吸われてリダイレクトが機能しない
   {
     path: '*',
     component: PageNotFound
@@ -91,5 +83,19 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if(store.getters.isAuthenticated) {
+      next({
+        path: '/memoLogin', query: { redirect: to.fullPath }
+      });
+    } else {
+      next()
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
