6cc964890fe8e9f4e8270918f74c39c6a1a2f44e
ryota kise <ryota1839@gmail.com>
2020-05-24 02:26:02 +0900

    fix:テスト環境を整えた

--- src/Greeting.spec.ts
+++ src/Greeting.spec.ts
@@ -1,5 +1,5 @@
 import { mount } from "@vue/test-utils";
-import Greeting from "@/components/Greeting.vue";
+import Greeting from './Greeting.vue';
 
 describe("Greeting.vue", () => {
   it("renders a greeting", () => {
