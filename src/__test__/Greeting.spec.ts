ce634505d32afbfab516414f4990369d74cd551c
ryota kise <ryota1839@gmail.com>
2020-05-24 11:17:23 +0900

    fix:vue/cli-plugin-unit-jestを入れてテストが動くように

--- src/__test__/Greeting.spec.ts
+++ src/__test__/Greeting.spec.ts
@@ -1,5 +1,5 @@
 import { mount } from "@vue/test-utils";
-import Greeting from './Greeting.vue';
+import Greeting from '../components/Greeting.vue';
 
 describe("Greeting.vue", () => {
   it("renders a greeting", () => {
