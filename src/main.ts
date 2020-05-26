e12d08b671bbcc264de9f9ff23081b4e13ebc6df
ryota kise <ryota1839@gmail.com>
2020-05-26 16:22:04 +0900

    fix:謎のエラー

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,9 @@ import store from './store';
 import vuetify from './plugins/vuetify';
 import Firebase from '@/firebase/firebase';
 
-import MemoHome from '@/views/memo/MemoHome.vue'
-import MemoAdd from '@/views/memo/MemoAdd.vue'
-import MemoDetail from '@/views/memo/MemoDetail.vue'
-import MemoEdit from '@/views/memo/MemoEdit.vue'
-import MemoSearch from '@/views/memo/MemoSearch.vue'
-import MemoLogin from '@/views/user/MemoLogin.vue'
-import MemoRegister from '@/views/user/MemoRegister.vue'
-
 Firebase.init();
 
 Vue.config.productionTip = false
-
-Vue.component("memo-home", MemoHome);
-Vue.component("memo-add", MemoAdd);
-Vue.component("memo-detail", MemoDetail);
-Vue.component("memo-edit", MemoEdit);
-Vue.component("memo-search", MemoSearch);
-Vue.component("memo-login", MemoLogin);
-Vue.component("memo-register", MemoRegister);
 
 new Vue({
   router,
