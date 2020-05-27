35ae706cb5d70c6593081f3fe0717e18186383b6
ryota kise <ryota1839@gmail.com>
2020-05-27 23:13:27 +0900

    add:memoSearchを分離するためにファイルを追加

---
+++ src/components/atoms/searchBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-text-field
+    v-model.number='search_term'
+    placeholder='search brand by code (ex 9984'
+    class='term'
+    :rules='codeRules'
+  />
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import * as rules from '@/config/user/rules';
+@Component
+export default class SearchBar extends Vue {
+search_term: number | null = null
+get codeRules() { return rules.codeRules }
+}
+</script>
---
+++ src/components/atoms/searchButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-btn
+    @click='filteredList()'
+    text
+    outlined
+    class='mt-2 filter'
+  >
+    検索
+  </v-btn>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class SearchButton extends Vue {}
+</script>
