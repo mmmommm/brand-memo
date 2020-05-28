fc40a37c98c0648934dac31e4db48c8866dcbbab
ryota kise <ryota1839@gmail.com>
2020-05-28 10:57:10 +0900

    fix:ビルド時のMBを変更

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
   ],
   chainWebpack: (config) => {
     config.plugin("fork-ts-checker").tap((args) => {
-      args[0].memoryLimit = 512;
+      args[0].memoryLimit = 1028;
       return args;
     });
   }
