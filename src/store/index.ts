96d70112008d0c4a9db2f06c2226be39b64f1dd3
ryota kise <ryota1839@gmail.com>
2020-06-22 01:19:37 +0900

    add: googleloginのmutation

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import firebase from 'firebase/app'
 import { firebaseauth } from '@/firebase/firebaseAuth';
+import { firestore } from '@/firebase/fireStore';
 import router from '@/router';
 
 Vue.use(Vuex)
@@ -23,6 +25,24 @@ export default new Vuex.Store({
     }
   },
   actions: {
+    googleLogin({ commit }) {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebaseauth.signInWithPopup(provider)
+        .then((result: any) => {
+          const userData = {
+            id: result.user.uid,
+            name: result.additionalUserInfo.profile.given_name,
+            email: result.additionalUserInfo.profile.email,
+          }
+          console.log(userData)
+          firestore.collection('users').doc(result.user.uid).set(userData)
+          commit('setIsAuthenticated', true)
+          router.push('/')
+        })
+        .catch(() => {
+          router.push('/memoLogin')
+        })
+    },
     userLogin({ commit }, { email, password }) {
       firebaseauth
         .signInWithEmailAndPassword(email, password)
@@ -41,6 +61,9 @@ export default new Vuex.Store({
       firebaseauth
         .createUserWithEmailAndPassword(email, password)
         .then(user => {
+          const userData = {
+            email: user.additionalUserInfo
+          }
           commit('setUser', user);
           commit('setIsAuthenticated', true);
           router.push('/');
