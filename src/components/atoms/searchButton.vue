909783da6cc7479613595361669ce229b524d35a
ryota kise <ryota1839@gmail.com>
2020-06-24 18:01:59 +0900

    fix: anyを消した

--- src/components/atoms/searchButton.vue
+++ src/components/atoms/searchButton.vue
@@ -11,15 +11,14 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import { firestore } from '@/firebase/fireStore';
-import { MemoItem } from '@/interface/memoItem';
 @Component
 export default class SearchButton extends Vue {
-  filteredData: MemoItem[] = []
+  filteredData: Array<firebase.firestore.DocumentData> = []
   filteredList() {
     firestore.collection('memos').where('code', '==', this.$store.state.searchTerm).get()
       .then((querySnapshot) => {
-        const s: MemoItem[] =[]
-        querySnapshot.forEach((doc: any) => {
+        const s: Array<firebase.firestore.DocumentData> =[]
+        querySnapshot.forEach((doc) => {
           s.push(doc.data())
         });
         this.filteredData = s
