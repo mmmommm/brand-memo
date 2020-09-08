f4314c3b1e55b58df231c56ab2aaff27184ef9f3
mmmommm <ryota1839@gmail.com>
2020-09-08 15:12:59 +0900

    不必要な関数を削除

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,8 +2,3 @@ import Firebase from 'firebase/app';
 import "firebase/auth";
 import credentials from './credentials';
 export const firebaseApp = Firebase.initializeApp(credentials);
-export default {
-  // init() {
-  //   Firebase.auth().setPersistence(Firebase.auth.Auth.Persistence.SESSION);
-  // }
-}
