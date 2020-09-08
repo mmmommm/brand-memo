4eba76cc13925e05d4324af3a627b45e67af5e05
mmmommm <ryota1839@gmail.com>
2020-09-08 11:26:00 +0900

    CI実行で落ちるため解決のために並列実行をoffに

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
 module.exports = {
+  //CI上での並列実行をoffに
+  parallel: false,
+  //明示的にCI実行時にログを表示するように
+  build: {
+    quiet: false
+  },
+  //vuetifyに環境依存していることを明示
   "transpileDependencies": [
     "vuetify"
   ],
+  //メモリの上限をいじった
   chainWebpack: (config) => {
     config.plugin("fork-ts-checker").tap((args) => {
       args[0].memoryLimit = 2048;
