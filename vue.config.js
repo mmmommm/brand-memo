87c38af1559e614c0326cf24a4d452acda8b9f3a
mmmommm <ryota1839@gmail.com>
2020-09-08 11:30:11 +0900

    buildオプションでlintで落ちたため消去

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,6 @@ module.exports = {
   //CI上での並列実行をoffに
   parallel: false,
   //明示的にCI実行時にログを表示するように
-  build: {
-    quiet: false
-  },
   //vuetifyに環境依存していることを明示
   "transpileDependencies": [
     "vuetify"
