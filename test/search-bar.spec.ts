3cf21d2230a2e32763faf10449a24e7311501998
mmmommm <ryota1839@gmail.com>
2020-09-09 14:32:00 +0900

    ボタンの動作を確認するテストファイルを追加

--- test/search-bar.spec.ts
+++ test/search-bar.spec.ts
@@ -1,24 +1,23 @@
 
 import Vuetify from 'vuetify'
 
-import SearchButton from '@/components/atoms/search-button.vue'
+import SearchBar from '@/components/atoms/search-bar.vue'
 
 import { mount, createLocalVue } from '@vue/test-utils'
 
 const localVue = createLocalVue()
 const vuetify = new Vuetify()
 
-describe('DeleteButton.vue', () => {
-  it('ボタンの動作確認', () => {
-    const wrapper = mount(SearchButton, {
+describe('SearchBar.vue', () => {
+  it('バリデーションの動作確認', () => {
+    const wrapper = mount(SearchBar, {
       localVue,
       vuetify
     })
     const vm = wrapper.vm
-    const event = jest.fn()
-    const button = wrapper.find('.v-btn')
-
-    console.log(button)
+    wrapper.setData({ searchTerm: 99845 })
+    vm.$emit('filteredList')
+    expect(wrapper.emitted().filteredList).toBeTruthy()
     // vm.$on('action-btn:clicked', event)
     // expect(event).toHaveBeenCalledTimes(0)
     // button.trigger('click')
