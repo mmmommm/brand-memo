46871fbbd2e4ed1122850fb751ff042c74c17c99
mmmommm <ryota1839@gmail.com>
2020-09-10 11:31:14 +0900

    コンポーネントを分離

---
+++ src/components/molecules/logout-button.vue
@@ -0,0 +1,24 @@
+<template>
+  <ModalWindow @approve="logout" @cancel="cancel">
+    <template #buttonTitle>Logout</template>
+    <template #modalTitle>本当にログアウトしてよろしいですか？</template>
+  </ModalWindow>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import ModalWindow from '@/components/atoms/modal-window.vue'
+@Component({
+  components: {
+    ModalWindow
+  }
+})
+export default class LogoutButton extends Vue {
+  logout() {
+    this.$store.commit('setDialog', false)
+    this.$store.dispatch('userLogout')
+  }
+  cancel() {
+    this.$store.commit('setDialog', false)
+  }
+}
+</script>
